8b6861c193abacb8f7499184e33f3f9148a997c3
Monk <sabintheworld@gmail.com>
2017-07-24 20:20:38 -0700

    Populates Modal
    Opening a model now populates that modal with the correct information.

--- scripts/surge.js
+++ scripts/surge.js
@@ -1,20 +1,6 @@
-﻿//SURGE KIOSK
-
-
-/*
-
-    Need to grab and JSON.serialize() the projects item from LocalStorage when you open the site
-    and shoves em in a variable,
-    then loop through the projects and populate the view with the data. 
-
-    When you click on a project it opens up a modal that should show more detail on the project. DONT CREATE
-    A MODAL FOR EACH ITEM. THATS LAME. When you click an item, it should populate a variable with the selected 
-    item's data and shove that stuff onto the modal.
-
-    We also need another page to hold the File Input thingy so it doesn't look so naysty. 
-
+﻿/*
+    We need another page to hold the File Input thingy so it doesn't look so naysty. 
     - Sabin 7/24
-
 */
 let projects = [];
 $(document).ready(function() {
@@ -97,10 +83,6 @@ function CSVtoArray(text) {
 }
 
 function populateProjects() {
-    let len = projects.length / 2;
-    let col1 = projects.slice(0, len);
-    let col2 = projects.slice(len);
-    console.log(projects)
     for (let i = 0; i < projects.length; i++) {
         $(".col-" + (i % 2 + 1)).append(
             '<div class="alert alert-warning" onclick="populateModal(\'' + projects[i].project_name + '\' )" data-toggle="modal" data-target="#myModal" role="alert">' +
@@ -112,5 +94,11 @@ function populateProjects() {
 
 function populateModal(name) {
     let currentModal = projects[_.findKey(projects, { 'project_name': name })];
-
+    $('#projectName').text(currentModal.project_name);
+    $('#studentNames').text(currentModal.students);
+    $('#advisorNames').text(currentModal.advisors);
+    $('#projectDescription').text(currentModal.project_description);
+    $("#advisorPhoto").attr("src", '../images/' + currentModal.advisor_photo);
+    $("#studentPhoto").attr("src", '../images/' + currentModal.student_photo);
+    $("#projectPhoto").attr("src", '../images/' + currentModal.project_photo);
 }
